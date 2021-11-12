a6fe7c582d891de7b69267470732f43f8c107746
hangnguyen <myhangzenda@gmail.com>
2021-11-12 11:03:17 +0700

    addFood

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
 
   def home
   end
-
+  def food 
+  end
+  def drink
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
   root 'pages#home'
+  get 'pages/food'
+  get 'pages/drink'
+
 
 end
