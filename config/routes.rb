822c8f7bd2995612ec16010ffbedac997ea11bff
thongth04 <thongth0497@gmail.com>
2021-11-15 20:14:55 +0700

    [Devise] Customize flash message

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,18 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   
+  # CUSTOMIZE DEVISE ROUTES
   get '/users/sign_in', to: redirect('login')
-  get '/users/sign_up', to: redirect('signup')
-  # get '/users/edit', to: "devise/registrations#edit"
-
-  devise_for :users, skip: [:sessions, :registrations]
-
+  # get '/users/sign_up', to: redirect('signup')
+  devise_for :users, skip: [:sessions]
   devise_scope :user do
     get    'login',  to: "devise/sessions#new",         as: :new_user_session
     post   'login',  to: "devise/sessions#create",      as: :user_session
     delete 'logout', to: "devise/sessions#destroy",     as: :destroy_user_session
  
-    get    'signup',  to: "devise/registrations#new",    as: :new_user_registration
-    post   'signup',  to: "devise/registrations#create", as: :user_registration
-    get    '/users/edit', to: "devise/registrations#edit", as: :edit_user_registration
+    # get    'signup',  to: "devise/registrations#new",    as: :new_user_registration
+    # post   'signup',  to: "devise/registrations#create", as: :user_registration
+    # get    '/users/edit', to: "devise/registrations#edit", as: :edit_user_registration
   end
 
 end
