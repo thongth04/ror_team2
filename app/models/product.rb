f602d74dca1ba67cde3936ca5082e7a5c5af1ce1
thinhnguyen2110 <thinhnguyen21102x@gmail.com>
2021-12-09 11:23:01 +0700

    EditPayments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
 
         def self.fill_out_attr
-            products = Product.select{ |product| product[:product_type] != 'food' }
+            products = Product.select{ |product| product[:product_type] != 'drink' }
             for product in products
-            product[:product_type] = "drink"
+            product[:product_type] = "food"
             product.save
             end
         end
