d38642e8fca845863b7567f6355a8bb0547adb14
thongth04 <thongth0497@gmail.com>
2021-12-15 10:34:40 +0700

    [Admin] Refactor views directory structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,11 @@ class PagesController < ApplicationController
   end
 
   def foods
-    @foods = Product.where(product_type: 'food').paginate(:page => params[:page], :per_page => 16).order('created_at desc')
+    @foods = Product.where(product_type: 'food').paginate(:page => params[:page], :per_page => 16)
   end
   
   def drinks
-    @drinks = Product.where(product_type: 'drink').paginate(:page => params[:page], :per_page => 16).order('created_at desc')
+    @drinks = Product.where(product_type: 'drink').paginate(:page => params[:page], :per_page => 16)
   end
+  
 end
