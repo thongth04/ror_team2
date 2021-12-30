53d17ba2bb623f983ded9d990e5cc4c92176ea2f
thongth04 <thongth0497@gmail.com>
2021-12-30 10:45:03 +0700

    [RoleAdmin] Add admin column to users and restrict actions from UI

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items
+
+  # attribute :price, default: 0
+  # attribute :quantity, default: 0
+
+  # VALID_CATEGORIES = ['Drinks', 'Foods', 'Others']
+
+  # validates :title, presence: true, length: { minimum: 5, maximum:40 }
+  # validates :price, presence: true
+  # validates :product_type, inclusion: { in: VALID_CATEGORIES }
+  # validates :quantity, presence: true
+  # validates :image_url, presence: true
+
 end
 
